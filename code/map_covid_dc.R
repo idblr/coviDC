96b68a29891a053d4c75ec35cd0bdfffd3a233ae
Ian Buller, PhD, MA <ian.buller@nih.gov>
2020-05-16 00:28:58 -0400

    Updated map_covid_dc.R
    ggplot2 and leaflet for DC case rate May 14, 2020

--- code/map_covid_dc.R
+++ code/map_covid_dc.R
@@ -16,8 +16,14 @@
 # PACKAGES #
 # -------- #
 
-loadedPackages <- c("ggmap", "googlesheets4", "grid", "htmlwidgets", "leaflet", "raster", "RColorBrewer", "rgdal", "sp")
+loadedPackages <- c("ggmap", "googlesheets4", "grid", "htmlwidgets", "latticeExtra", "leaflet", "raster", "RColorBrewer", "rgdal", "sp", "stringr")
 invisible(lapply(loadedPackages, require, character.only = T))
+
+# -------- #
+# SETTINGS #
+# -------- #
+
+gs4_deauth() # because we are not reading a public repo
 
 # ---------------- #
 # DATA IMPORTATION #
@@ -29,171 +35,109 @@ gis_path <- "https://opendata.arcgis.com/datasets/de63a68eb7674548ae0ac01867123f
 dc <- geojsonio::geojson_read(gis_path,  what = "sp")
 #plot(dc) # check importation, uncomment for plot
 
-# https://docs.google.com/spreadsheets/d/1u-FlJe2B1rYV0obEosHBks9utkU30-C2TSkHka6AVS8/edit#gid=1923705378
+# District of Columbia SAR-CoV-2 Data prepared by @zmotoly
+covid_path <- "https://docs.google.com/spreadsheets/d/1u-FlJe2B1rYV0obEosHBks9utkU30-C2TSkHka6AVS8/edit#gid=1923705378"
+covid <- googlesheets4::read_sheet(ss = covid_path,
+                                   sheet = 2, # second sheet
+                                   skip = 1) # skip 1st row of annotation
+names(covid) <- sub("\n", "", names(covid)) # remove extra line in column names
+names(covid) <- gsub(" ", "_", names(covid)) # replace spaces with underscore
 
 # ---------------------- #
 # GEOGRAPHIC PREPARATION #
 # ---------------------- #
 
-# Only Ward 5
-ward5 <- dc_ward[dc_ward$WARD_ID == 5,]
-
-# Restrict zipcodes to Ward 5
-ward5_zip <- raster::crop(dc_zip, ward5) 
-
-ward5_block <- raster::crop(dc_block, ward5)
-
-# Create Random Addresses
-dc_address <- sp::spsample(ward5, n = 100, "random")
-
-plot(ward5)
-plot(ward5_zip, add = T)
-plot(dc_address, add = T)
-plot(ward5_block, add = T)
-
-
-dc_address@coords
-
-# Extract zipcode at each point
-dc_address$zipcode <- sp::over(dc_address,dc_zip)$ZIP_CODE 
-
-# Count addresses within each zipcode
-zip_count <- as.data.frame(table(dc_address$zipcode)) 
-
-# Merge counts per zipcode to zipcode data frame
-ward5_zip@data <- data.frame(ward5_zip@data, zip_count[match(ward5_zip@data[,"ZIPCODE"], zip_count[,"Var1"]),])
-
-# --------------------- #
-#### STATIC PLOTTING ####
-# --------------------- #
+# Merge
+dc_covid <- sp::merge(dc, covid, by.x = "CODE", by.y = "NB_Code")
 
 # UTM Coordinate Reference System for meter distances (to include scale)
-ward5_zip_proj <- spTransform(ward5_zip, CRS("+init=EPSG:32618")) # UTM zone 18N (Washington, DC)
+dc_covid_proj <- sp::spTransform(dc_covid, CRS("+init=EPSG:32618")) # UTM zone 18N (Washington, DC)
 
-# Colorkey scaled by count per zipcode
-at <- seq(from = 0, to = max(ward5_zip$Freq), by = 3) + 1 # set breaks in colorkey (must be one more than default to fit min and max values)
-at_names <- seq(from = 0, to = max(ward5_zip$Freq), by = 3) # set name of breaks in colorkey
+# --------------- #
+# STATIC PLOTTING #
+# --------------- #
 
-# Custom scale
-scale <- list("SpatialPolygonsRescale",
-              layout.scale.bar(),
-              scale = 1000, # 1000 meters
-              fill = c("transparent", "black"), # colors of scale
-              offset = c(325500, 4307500) # location in plot
-)
+### helpful material: https://cengel.github.io/rspatial/4_Mapping.nb.html
+### Data preparation, ggplot2 requires a data.frame
+dc_covid_df <- broom::tidy(dc_covid_proj) # convert to tidy data frame
+dc_covid_proj$polyID <- sapply(slot(dc_covid_proj, "polygons"), function(x) slot(x, "ID")) # preserve polygon id
+CoV_DC_df <- merge(dc_covid_df, dc_covid_proj, by.x = "id", by.y="polyID") # merge data
 
-# The scale argument sets length of bar in map units
-text1 <- list("sp.text", c(325500, 4307650), "0 km", cex = 1*f) # lower limit set just above scale
-text2 <- list("sp.text", c(326500, 4307650), "1 km", cex = 1*f) # upper limit set just above scale
-
-# Custom compass rose
-arrow <- list("SpatialPolygonsRescale", 
-              sp::layout.north.arrow(),
-              scale = 500, # size of plot 
-              offset = c(325900, 4307700) # location in plot (just above and middle of scale)
-)
-
-# GREYSCALE
-f <- 2 # expansion factor (with increasing size of the plot)
-png(file = "figures/ward5_zip_greyscale.png", width = 2000, height = 1600) # turn on .png device
-sp::spplot(ward5_zip_proj, # data
-           "Freq", # column name
-           col.regions = rev(gray.colors(length(at))), # color palette
-           at = at + 1, # set breaks one more than default
-           par.settings = list(axis.line = list(col =  'transparent')), # remove default box around plot
-           colorkey = list(labels = list(at = at + 1, # set breaks one more than default (to include min and max values on colorkey)
-                                         labels = at_names, # set name of breaks to match data
-                                         cex = 2*f, # size of labels of colorkey
-                                         fontface = 1, # set font style (normal)
-                                         #fontfamily = 'LM Roman 10', # set font name
-                                         legend = "frequency" # label of colorkey
-           )
-           ),
-           sub = list(label = "Copywrite Ian Buller", cex = 1*f), # subtitle for credit
-           sp.layout = list(scale, text1, text2, arrow) # additions: scale and north arrow
-)
-grid::grid.text("Frequency", # for overall label of colorkey
-                x = grid::unit(0.95, "npc"), # x position
-                y = grid::unit(0.50, "npc"), # y position
-                rot = 90, # for vertical colorkey
-                gp = grid::gpar(fontsize = 20*f # for custom size of overall label
-                                #,fontfamily= 'LM Roman 10' # for custom font of overall label
-                )
-)
-dev.off() # turn off device
-
-# WITH COLOR
-png(file = "figures/ward5_zip_wcolor.png", width = 2000, height = 1600)
-sp::spplot(ward5_zip_proj,
-           "Freq",
-           col.regions = rev(terrain.colors(length(at))), # also: heat.colors (red) or create custom palette with RColorBrewer
-           at = at + 1,
-           par.settings = list(axis.line = list(col =  'transparent')),
-           colorkey = list(labels = list(at = at + 1,
-                                         labels = at_names,
-                                         cex = 2*f, 
-                                         fontface = 1, # set font style (normal)
-                                         #fontfamily = 'LM Roman 10', # set font name
-                                         legend = "frequency")
-           ),
-           sub = list(label = "Copywrite Ian Buller", cex = 1*f),
-           sp.layout = list(scale, text1, text2, arrow)
-)
-grid::grid.text("Frequency", # for overall label of colorkey
-                x = grid::unit(0.95, "npc"), # x position
-                y = grid::unit(0.50, "npc"), # y position
-                rot = 90, # for vertical colorkey
-                gp = grid::gpar(fontsize = 20*f # for custom size of overall label
-                                #,fontfamily= 'LM Roman 10' # for custom font of overall label
-                )
-)
+### Plot of cumulative cases per capita
+f <- 1 # exansion factor
+png(file = "figures/COVID_DC_Cumulative_per1000_ggplot.png", height = 1000*f, width = 1000*f)
+ggplot2::ggplot() +                                      # initialize ggplot object
+  ggplot2::geom_polygon(                                 # make a polygon
+    data = CoV_DC_df,                                    # data frame
+    ggplot2::aes(x = long, y = lat, group = group,       # coordinates, and group them by polygons
+                 fill = ggplot2::cut_interval(Cases_per_1000_May_14, 10)),       # variable to use for filling
+    colour = "white") +                                  # color of polygon borders
+  ggplot2::scale_fill_brewer("Cumulative cases per 1,000",# title of colorkey 
+                             palette = "Purples",          # fill with brewer colors 
+                             na.value = "grey67",
+                             direction = 1,             # reverse colors in colorkey
+                             guide = ggplot2::guide_legend(reverse = T)) +  # reverse order of colokey
+  ggplot2::ggtitle("Cumulative SARS-CoV-2 cases per 1,000 (January 22, 2020 - May 14, 2020)") + # add title
+  ggplot2::theme(line = ggplot2::element_blank(),        # remove axis lines
+                 axis.text = ggplot2::element_blank(),            # remove tickmarks
+                 axis.title = ggplot2::element_blank(),           # remove axis labels
+                 panel.background = ggplot2::element_blank(),     # remove background gridlines
+                 text = ggplot2::element_text(size = 15*f)) +     # set font size
+  ggplot2::coord_equal()                                 # both axes the same scale
 dev.off()
 
-## LEAFLET
+## Leaflet Plot
+### Work with unprojected spatialpolygonsdataframe CoV_GA_pop
+### Project to WGS84 EPSG:4326
+CoV_DC_WGS84 <- sp::spTransform(dc_covid_proj, CRS("+init=epsg:4326"))
 
-ward5_zip$popup <- paste(ward5_zip$ZIPCODE, ": ", ward5_zip$Freq, " patient(s)", sep = "")
+dc_health <- stringr::str_to_title(CoV_DC_WGS84$Neighborhood_Name)
+dc_health[c(11,25,41,49)] <- c("DC Medical Center", "GWU", "National Mall", "SW/Waterfront" )
 
+### Popups
+CoV_DC_WGS84$popup1 <- paste(dc_health, ": ", format(round(CoV_DC_WGS84$Total_cases_May_14, digits = 0), big.mark = ",", trim = T), " cumulative cases", sep = "")
+CoV_DC_WGS84$popup2 <- paste(dc_health, ": ", format(round(CoV_DC_WGS84$Cases_per_1000_May_14, digits = 0), big.mark = ",", trim = T), " cumulative cases per 1,000", sep = "")
 
-# Palette
-pal_zcta <- colorNumeric(palette = "Blues", domain = ward5_zip$Freq)
+### Palettes
+pal_cum <- leaflet::colorNumeric(palette = "Greys", domain = CoV_DC_WGS84$Total_cases_May_14)
+pal_rate <- leaflet::colorNumeric(palette = "Greys", domain = CoV_DC_WGS84$Cases_per_1000_May_14)
 
-# Icon
-leafIcons <- icons(
-  iconUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Featured_articles_star_modern.svg/800px-Featured_articles_star_modern.svg.png",
-  iconWidth = 15, iconHeight = 15,
-  iconAnchorX = 0, iconAnchorY = 0)
-
-# Icon legend
-html_legend <- "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Featured_articles_star_modern.svg/800px-Featured_articles_star_modern.svg.png'style='width:20px;height:20px;'> Patient"
-
-# Create leaflet plot
-dc_m1 <- leaflet::leaflet(ward5_zip) %>% # initial data = location of ZCTAs
-  leaflet::setView(lng = -77, lat = 38.92, zoom = 12) %>% # starting location (Washington DC)
+### Create leaflet plot
+dc_m1 <- leaflet::leaflet(CoV_DC_WGS84) %>% # initial data = Georiga county-level COVID-19 data
+  leaflet::setView(lng = -83, lat = 32, zoom = 7) %>% # starting location (Georgia, USA)
   leaflet::addProviderTiles(providers$OpenStreetMap.BlackAndWhite, group = "OSM (Default)") %>% # default basemap
   leaflet::addProviderTiles(providers$Esri.WorldTopoMap, group = "Terrain") %>% # additional basemap
   leaflet::addProviderTiles(providers$OpenTopoMap, group = "Topographic") %>% # additional basemap
-  leaflet::addPolygons(data = ward5_zip, color = "black", weight = 1, smoothFactor = 0.5, opacity = 1,
-                       fillOpacity = 0.5, fillColor = ~pal_zcta(Freq), popup = ~popup, # stroke = FALSE
+  leaflet::addPolygons(data = CoV_DC_WGS84, color = "black", weight = 1, smoothFactor = 0.5, opacity = 1,
+                       fillOpacity = 0.5, fillColor = ~pal_cum(Total_cases_May_14), popup = ~popup1, # stroke = FALSE
                        highlightOptions = highlightOptions(color = "white", weight = 2, bringToFront = TRUE),
-                       group = "By Zipcode"
+                       group = "Cumulative Cases"
   ) %>%
-  leaflet::addMarkers(lng = dc_address@coords[,1], lat = dc_address@coords[,2], icon = leafIcons,
-                      group = "By Address"
+  leaflet::addPolygons(data = CoV_DC_WGS84, color = "black", weight = 1, smoothFactor = 0.5, opacity = 1,
+                       fillOpacity = 0.5, fillColor = ~pal_rate(Cases_per_1000_May_14), popup = ~popup2, # stroke = FALSE
+                       highlightOptions = highlightOptions(color = "white", weight = 2, bringToFront = TRUE),
+                       group = "Cumulative Cases"
   ) %>%
   leaflet::addLayersControl(baseGroups = c("OSM (Default)", "Terrain", "Topographic"),
-                            overlayGroups = c("By Zipcode", "By Address"),
+                            overlayGroups = c("Cumulative Cases", "Cumulative Rate"),
                             options = layersControlOptions(collapsed = FALSE)
   ) %>% # layer selection
-  addLegend("topright", pal = pal_zcta, values = ~Freq,
-            title = "Frequency",
+  addLegend("topright", pal = pal_cum, values = ~Total_cases_May_14,
+            title = "Cumulative Cases",
             opacity = 1,
-            group = "By Zipcode") %>%
-  addControl(html = html_legend, position = "topright") %>%
-  leaflet::hideGroup(c("By Zipcode", "By Address")) %>% # no data shown (default)
+            group = "Cumulative Cases") %>%
+  addLegend("topright", pal = pal_rate, values = ~Cases_per_1000_May_14,
+            title = "Cumulative Rate per 1,000",
+            opacity = 1,
+            group = "Cumulative Rate") %>%
+  leaflet::hideGroup(c("Cumulative Cases", "Cumulative Rate")) %>% # no data shown (default)
   leaflet::addMiniMap() # add mini map
 dc_m1 # display leaflet plot
 
-# Export
-htmlwidgets::saveWidget(dc_m1, file = "dc_ward5.html", selfcontained = TRUE)
+###############
+# EXPORTATION #
+###############
 
+# export leaflet plot
+htmlwidgets::saveWidget(dc_m1, file = "COVID_DC_Leaflet.html", selfcontained = TRUE)
 # -------------------------- END OF CODE -------------------------- #  
