546cb8249cbeaba4b6ef612d7ceac11cd374b30d
Ian Buller, PhD, MA <ian.buller@nih.gov>
2020-05-16 22:08:34 -0400

    Updated README.md and cleaned up files

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
-# DCgeo
+# coviDC
 
-R code for static and interactive geographical maps of the District of Columbia
+## Maps of SARS-CoV-2 Data in the District of Columbia (2020) Using R 
  
+Open-source code to download, manage, and visualize publicly available COVID-19 epidemiologic data in the District of Columbia. See this [blog post](https://idblr.rbind.io/post/2020-05-15-covid-dc/covid-dc/) for an example of maps for May 15, 2020.
+
+### Data Sources
+
+1. DC Mayoral Office [coronavirus data](https://coronavirus.dc.gov/page/coronavirus-data)
+2. Collated daily cumulative case count and case rates (per 1,000) by Molly Tolzmann [zmotoly](https://twitter.com/zmotoly) available on a public [Google Sheet]("https://docs.google.com/spreadsheets/d/1u-FlJe2B1rYV0obEosHBks9utkU30-C2TSkHka6AVS8/edit#gid=1923705378").
+3. Presented at the [DC health planning neighborhood level](https://opendata.dc.gov/datasets/dc-health-planning-neighborhoods)
+
+#### Important Notes
+* Case data does not account for degree of testing across DC
+* Case rate based on the 2018 American Community Survey (ACS) census tract data available from [OpenData DC](https://opendata.dc.gov/datasets/acs-2018-population-variables-tract) at the Census Tract level.
+* Case data are cumulative since beginning of the COVID-19 outbreak in DC.
+
+### Getting Started
+
+The R workflow was built with `R version 3.6.3 (2020-02-29) -- "Holding the Windsock"` and requires several established `R` packages. 
+
+```
+  loadedPackages <- c("broom", "geojsonio", "ggplot2", "googlesheets4", "htmlwidgets", "leaflet", "sp", "stringr")
+  invisible(lapply(loadedPackages, require, character.only = T))
+```
+
+Follow the [map_covid_dc.R](https://github.com/idblr/coviDC/blob/master/code/map_covid_dc.R) file to generate the example maps from May 15, 2020.
+
+### In Development
+
+We enough temporal data, the change in COVID-19 cases per 1,000 would be possible to estimate across DC and may indicate recent upticks in cases. Stay tuned!
+
+### Authors
+
+* **Ian Buller** - *Initial heavycoding with expertise in spatial statistics* - [GitHub](https://github.com/idblr)
+
+See also the list of [contributors](https://github.com/idblr/coviDC/graphs/contributors) who participated in this project.
+
+### License
+
+This project is licensed under the Apache 2.0 License - see the [LICENSE.md](https://github.com/idblr/coviDC/blob/master/LICENSE) file for details
+
+### Acknowledgments
+
+* Huge s/o to Molly Tolzmann [zmotoly](https://twitter.com/zmotoly) for the data collation and initial inspiration from [her original post](https://www.popville.com/2020/05/dc-neighborhood-covid-coronavirus-map-population/#more-234053) on [PoPville](https://twitter.com/PoPville).
+* Big thanks to the team at [Open Data DC](https://opendata.dc.gov/)
+* And to the DC Mayoral Office for the publicly avaiable [coronavirus data](https://coronavirus.dc.gov/page/coronavirus-data) and lcoal leadership
--- code/map_covid_dc.R
+++ code/map_covid_dc.R
@@ -17,7 +17,7 @@
 # PACKAGES #
 # -------- #
 
-loadedPackages <- c("ggmap", "googlesheets4", "grid", "htmlwidgets", "latticeExtra", "leaflet", "raster", "RColorBrewer", "rgdal", "sp", "stringr")
+loadedPackages <- c("broom", "geojsonio", "ggplot2", "googlesheets4", "htmlwidgets", "leaflet", "sp", "stringr")
 invisible(lapply(loadedPackages, require, character.only = T))
 
 # -------- #
@@ -66,77 +66,72 @@ dc_covid_proj$polyID <- sapply(slot(dc_covid_proj, "polygons"), function(x) slot
 CoV_DC_df <- merge(dc_covid_df, dc_covid_proj, by.x = "id", by.y="polyID") # merge data
 
 ## Plot of cumulative cases per 1,000
-f <- 1 # exansion factor
-png(file = "figures/COVID_DC_Cumulative_per1000_ggplot.png", height = 1000*f, width = 1000*f)
-ggplot2::ggplot() +                                      # initialize ggplot object
-  ggplot2::geom_polygon(                                 # make a polygon
-    data = CoV_DC_df,                                    # data frame
-    ggplot2::aes(x = long, y = lat, group = group,       # coordinates, and group them by polygons
-                 fill = ggplot2::cut_interval(Cases_per_1000_May_14, 10)),       # variable to use for filling
-    colour = "white") +                                  # color of polygon borders
-  ggplot2::scale_fill_brewer("Cumulative cases per 1,000",# title of colorkey 
-                             palette = "Purples",          # fill with brewer colors 
-                             na.value = "grey67",
-                             direction = 1,             # reverse colors in colorkey
+ggplot2::ggplot() +                                             # initialize ggplot object
+  ggplot2::geom_polygon(                                        # make a polygon
+    data = CoV_DC_df,                                           # data frame
+    ggplot2::aes(x = long, y = lat, group = group,              # coordinates, and group them by polygons
+                 fill = ggplot2::cut_interval(Cases_per_1000_May_15, 10)),       # variable to use for filling
+    colour = "white") +                                         # color of polygon borders
+  ggplot2::scale_fill_brewer("Cumulative cases per 1,000",      # title of colorkey 
+                             palette = "Purples",               # fill with brewer colors 
+                             na.value = "grey67",               # color for NA (The National Mall)
+                             direction = 1,                     # reverse colors in colorkey
                              guide = ggplot2::guide_legend(reverse = T)) +  # reverse order of colokey
-  ggplot2::ggtitle("Cumulative SARS-CoV-2 cases per 1,000 (March 7, 2020 - May 14, 2020)") + # add title
-  ggplot2::theme(line = ggplot2::element_blank(),        # remove axis lines
-                 axis.text = ggplot2::element_blank(),            # remove tickmarks
-                 axis.title = ggplot2::element_blank(),           # remove axis labels
-                 panel.background = ggplot2::element_blank(),     # remove background gridlines
-                 text = ggplot2::element_text(size = 15*f)) +     # set font size
-  ggplot2::coord_equal()                                 # both axes the same scale
-dev.off()
+  ggplot2::ggtitle("Cumulative SARS-CoV-2 cases per 1,000 (March 7, 2020 - May 15, 2020)") + # add title
+  ggplot2::theme(line = ggplot2::element_blank(),               # remove axis lines
+                 axis.text = ggplot2::element_blank(),          # remove tickmarks
+                 axis.title = ggplot2::element_blank(),         # remove axis labels
+                 panel.background = ggplot2::element_blank(),   # remove background gridlines
+                 text = ggplot2::element_text(size = 10)) +     # set font size
+  ggplot2::coord_equal()                                        # both axes the same scale
 
 # -------------------- #
 # INTERACTIVE PLOTTING #
 # -------------------- #
 
 # Uses leaflet package
-## Work with unprojected spatialpolygonsdataframe
+# Work with unprojected spatialpolygonsdataframe
 ## Project to WGS84 EPSG:4326
 CoV_DC_WGS84 <- sp::spTransform(dc_covid_proj, CRS("+init=epsg:4326"))
 
 ## Create Popups
 dc_health <- stringr::str_to_title(CoV_DC_WGS84$Neighborhood_Name)
 dc_health[c(11,25,41,49)] <- c("DC Medical Center", "GWU", "National Mall", "SW/Waterfront" )
-CoV_DC_WGS84$popup1 <- paste(dc_health, ": ", format(round(CoV_DC_WGS84$Total_cases_May_14, digits = 0), big.mark = ",", trim = T), " cumulative cases", sep = "")
-CoV_DC_WGS84$popup2 <- paste(dc_health, ": ", format(round(CoV_DC_WGS84$Cases_per_1000_May_14, digits = 0), big.mark = ",", trim = T), " cumulative cases per 1,000", sep = "")
+CoV_DC_WGS84$popup1 <- paste(dc_health, ": ",
+                             format(round(CoV_DC_WGS84$Total_cases_May_15, digits = 0), big.mark = ",", trim = T),
+                             " cumulative cases", sep = "")
+CoV_DC_WGS84$popup2 <- paste(dc_health, ": ",
+                             format(round(CoV_DC_WGS84$Cases_per_1000_May_15, digits = 0), big.mark = ",", trim = T),
+                             " cumulative cases per 1,000", sep = "")
 
 ## Set Palettes
-pal_cum <- leaflet::colorNumeric(palette = "Greys", domain = CoV_DC_WGS84$Total_cases_May_14)
-pal_rate <- leaflet::colorNumeric(palette = "Greys", domain = CoV_DC_WGS84$Cases_per_1000_May_14)
+pal_cum <- leaflet::colorNumeric(palette = "Purples",
+                                 domain = CoV_DC_WGS84$Total_cases_May_15,
+                                 na.color = "#555555")
+pal_rate <- leaflet::colorNumeric(palette = "Purples",
+                                  domain = CoV_DC_WGS84$Cases_per_1000_May_15,
+                                  na.color = "#555555")
 
 ### Create leaflet plot
-dc_m1 <- leaflet::leaflet(CoV_DC_WGS84) %>% # initial data = DC health planning neighborhood-level COVID-19 data
-  leaflet::setView(lng = -38.9, lat = 77.0, zoom = 7) %>% # starting location (DC, USA)
-  leaflet::addProviderTiles(providers$OpenStreetMap.BlackAndWhite, group = "OSM (Default)") %>% # default basemap
-  leaflet::addProviderTiles(providers$Esri.WorldTopoMap, group = "Terrain") %>% # additional basemap
-  leaflet::addProviderTiles(providers$OpenTopoMap, group = "Topographic") %>% # additional basemap
+dc_m1 <-leaflet::leaflet(CoV_DC_WGS84, width = "100%") %>%                        # initial data
+  leaflet::setView(lng = -77, lat = 38.9, zoom = 11) %>%                  # starting coordinates
+  leaflet::addTiles() %>% # basemap
   leaflet::addPolygons(data = CoV_DC_WGS84, color = "black", weight = 1, smoothFactor = 0.5, opacity = 1,
-                       fillOpacity = 0.5, fillColor = ~pal_cum(Total_cases_May_14), popup = ~popup1, # stroke = FALSE
+                       fillOpacity = 0.67, fillColor = ~pal_cum(Total_cases_May_15), popup = ~popup1,
                        highlightOptions = highlightOptions(color = "white", weight = 2, bringToFront = TRUE),
-                       group = "Cumulative Cases"
-  ) %>%
+                       group = "Cumulative Cases") %>%
   leaflet::addPolygons(data = CoV_DC_WGS84, color = "black", weight = 1, smoothFactor = 0.5, opacity = 1,
-                       fillOpacity = 0.5, fillColor = ~pal_rate(Cases_per_1000_May_14), popup = ~popup2, # stroke = FALSE
+                       fillOpacity = 0.67, fillColor = ~pal_rate(Cases_per_1000_May_15), popup = ~popup2,
                        highlightOptions = highlightOptions(color = "white", weight = 2, bringToFront = TRUE),
-                       group = "Cumulative Cases"
-  ) %>%
-  leaflet::addLayersControl(baseGroups = c("OSM (Default)", "Terrain", "Topographic"),
-                            overlayGroups = c("Cumulative Cases", "Cumulative Rate"),
-                            options = layersControlOptions(collapsed = FALSE)
-  ) %>% # layer selection
-  addLegend("topright", pal = pal_cum, values = ~Total_cases_May_14,
-            title = "Cumulative Cases",
-            opacity = 1,
-            group = "Cumulative Cases") %>%
-  addLegend("topright", pal = pal_rate, values = ~Cases_per_1000_May_14,
-            title = "Cumulative Rate per 1,000",
-            opacity = 1,
-            group = "Cumulative Rate") %>%
+                       group = "Cumulative Rate") %>%
+  leaflet::addLayersControl(overlayGroups = c("Cumulative Cases", "Cumulative Rate"), # layer selection
+                            options = layersControlOptions(collapsed = FALSE)) %>%     
+  addLegend("topright", pal = pal_cum, values = ~Total_cases_May_15,                  # legend for cases
+            title = "Cumulative Cases", opacity = 1, group = "Cumulative Cases") %>%
+  addLegend("topright", pal = pal_rate, values = ~Cases_per_1000_May_15,              # legend for rate
+            title = "Cumulative Rate per 1,000", opacity = 1, group = "Cumulative Rate") %>%
   leaflet::hideGroup(c("Cumulative Cases", "Cumulative Rate")) %>% # no data shown (default)
-  leaflet::addMiniMap() # add mini map
+  leaflet::addMiniMap(position = "bottomleft") # add mini map
 dc_m1 # display leaflet plot
 
 ###############
@@ -144,5 +139,5 @@ dc_m1 # display leaflet plot
 ###############
 
 # export leaflet plot
-htmlwidgets::saveWidget(dc_m1, file = "COVID_DC_Leaflet.html", selfcontained = TRUE)
+htmlwidgets::saveWidget(dc_m1, file = "COVID_DC_Leaflet.html")
 # -------------------------- END OF CODE -------------------------- #  
